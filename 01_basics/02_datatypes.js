ace43934264f2ca24b0d9d4e4064d609a12ed1b2
manishyadav150 <itsmkyadav15@gmail.com>
2026-07-13 14:04:28 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Manish")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//Primitive datatypes:
+// number => 2 to the power of 53
+// bigint
+// string => "" or ''
+// boolean => true/false
+// null => standalone value or representation of empty value
+// undefined => jab aapne value define nahi ki hai
+// symbol => unique
+
+//Non Primitive datatypes: 
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
